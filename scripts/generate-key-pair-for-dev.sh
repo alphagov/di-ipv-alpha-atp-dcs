3152c2e176e2241fed4490f4b3a71fb2060c7d44
martin-bucinskas <7595909+martin-bucinskas@users.noreply.github.com>
2021-06-09 11:49:01 +0100

    dcs-setup: Updating the cert gen to not present dialog.

--- scripts/generate-key-pair-for-dev.sh
+++ scripts/generate-key-pair-for-dev.sh
@@ -4,8 +4,8 @@ CERT_PATH="src/main/resources/certs"
 [[ -d $KEY_PATH ]] || mkdir $KEY_PATH
 [[ -d $CERT_PATH ]] || mkdir $CERT_PATH
 
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout $KEY_PATH/client-signing-key.pem -out $CERT_PATH/client-signing-cert.pem
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout $KEY_PATH/client-encryption-key.pem -out $CERT_PATH/client-encryption-cert.pem
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout $KEY_PATH/server-signing-key.pem -out $CERT_PATH/server-signing-cert.pem
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout $KEY_PATH/server-encryption-key.pem -out $CERT_PATH/server-encryption-cert.pem
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout $KEY_PATH/ipv-signing-key.pem -out $CERT_PATH/ipv-signing-cert.pem
+openssl req -newkey rsa:2048 -subj "/O=Digital Identity/CN=client-signing" -new -nodes -x509 -days 3650 -keyout $KEY_PATH/client-signing-key.pem -out $CERT_PATH/client-signing-cert.pem
+openssl req -newkey rsa:2048 -subj "/O=Digital Identity/CN=client-encryption" -new -nodes -x509 -days 3650 -keyout $KEY_PATH/client-encryption-key.pem -out $CERT_PATH/client-encryption-cert.pem
+openssl req -newkey rsa:2048 -subj "/O=Digital Identity/CN=server-signing" -new -nodes -x509 -days 3650 -keyout $KEY_PATH/server-signing-key.pem -out $CERT_PATH/server-signing-cert.pem
+openssl req -newkey rsa:2048 -subj "/O=Digital Identity/CN=server-encryption" -new -nodes -x509 -days 3650 -keyout $KEY_PATH/server-encryption-key.pem -out $CERT_PATH/server-encryption-cert.pem
+openssl req -newkey rsa:2048 -subj "/O=Digital Identity/CN=ipv-signing" -new -nodes -x509 -days 3650 -keyout $KEY_PATH/ipv-signing-key.pem -out $CERT_PATH/ipv-signing-cert.pem
