2c61624b9623d17aba1be909016d477a7b3104d6
martin-bucinskas <7595909+martin-bucinskas@users.noreply.github.com>
2021-06-21 12:40:51 +0100

    secrets: Using mTLS for the DCS call.
    - Setting up a key manager and a trust manager to use the DCS client key and cert, and DCS server cert.
    - Updated the application.yml to pull the TLS certs and key from env vars.
    - The local dev script updated to generate yet another key and certificate for mTLS.

--- src/main/java/uk/gov/di/ipv/atp/dcs/config/AtpConfig.java
+++ src/main/java/uk/gov/di/ipv/atp/dcs/config/AtpConfig.java
@@ -1,23 +1,30 @@
 package uk.gov.di.ipv.atp.dcs.config;
 
 import com.google.gson.Gson;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 import uk.gov.di.ipv.atp.dcs.domain.Thumbprints;
 import uk.gov.di.ipv.atp.dcs.utils.KeyReader;
 
-import java.security.Key;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import javax.net.ssl.SSLException;
+import java.security.*;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Base64;
 
+@Slf4j
 @Configuration
 public class AtpConfig {
 
@@ -25,12 +32,34 @@ public class AtpConfig {
     private @Value("${atp.ipv.signing.key}") String ipvSigningKey;
     private @Value("${atp.ipv.signing.cert}") String ipvSigningCert;
 
+    private @Value("${dcs.tls.client.key}") String tlsClientKey;
+    private @Value("${dcs.tls.client.cert}") String tlsClientCert;
+    private @Value("${dcs.tls.server.cert}") String tlsServerCert;
+
     @Bean("dcs-web-client")
-    WebClient webClient() {
+    WebClient webClient(@Qualifier("dcs-tls-ssl") SslContext sslContext) {
         var strippedBaseUrl = dcsBaseUrl.replaceAll("\"", "");
+
+        var connector = HttpClient.create().secure(spec -> spec.sslContext(sslContext));
+        var reactorConnector = new ReactorClientHttpConnector(connector);
+
         return WebClient
             .builder()
+            .clientConnector(reactorConnector)
             .baseUrl(strippedBaseUrl)
+            .build();
+    }
+
+    @Bean("dcs-tls-ssl")
+    SslContext sslContext() throws SSLException, NoSuchAlgorithmException, InvalidKeySpecException, CertificateException {
+        var clientKey = KeyReader.loadKey(tlsClientKey);
+        var clientCert = KeyReader.loadCertFromString(tlsClientCert);
+        var serverCert = KeyReader.loadCertFromString(tlsServerCert);
+
+        return SslContextBuilder
+            .forClient()
+            .keyManager((PrivateKey) clientKey, (X509Certificate) clientCert)
+            .trustManager((X509Certificate) serverCert)
             .build();
     }
 
